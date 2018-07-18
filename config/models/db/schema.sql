b5b568ef149e90c8f22454245dac8e3575455ec5
LiftoffSBC <eagillet@gmail.com>
2018-07-18 14:18:02 -0400

    added seeds file, updated schema

---
+++ config/models/db/schema.sql
@@ -0,0 +1,23 @@
+-- Schema
+DROP DATABASE IF EXISTS stock_db;
+CREATE DATABASE stock_db;
+
+USE stock_db;
+
+-- Our stock table to hold data from IEX API as added from user input
+CREATE TABLE stocks
+(
+    id int NOT NULL AUTO_INCREMENT,
+    symbol varchar(255) NOT NULL,
+    companyName VARCHAR(255),
+    latestPrice INTEGER,
+    related VARCHAR(255),
+    PRIMARY KEY (id)
+);
+
+-- --------//This is how the info comes in from IEX API//----------
+-- symbol string
+-- companyName string
+-- latestPrice  number
+-- related string
+-- --------//------------------------------------------//----------
