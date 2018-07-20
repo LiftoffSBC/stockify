13e3c0f5fa06d0d200e5be550d32fbda41461ff5
Trenton Ankenbruck <trenton15@outlook.com>
2018-07-20 17:40:40 -0400

    Moved .html files to /public/html per Jeff's recommendations

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,29 +1,11 @@
-var db = require("../models");
-
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.stocks.findAll({}).then(function(dbstocks) {
-      res.render("index", {
-        msg: "Welcome!",
-        stocks: dbstocks
-      });
-    });
-  });
-
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.stocks
-      .findOne({ where: { id: req.params.id } })
-      .then(function(dbstocks) {
-        res.render("stocks", {
-          stocks: dbstocks
-        });
-      });
+    res.sendFile(path.join(__dirname, "../public/html/index.html"));
   });
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
-    res.render("404");
+    res.status(404).send("404 - Page not found");
   });
 };
