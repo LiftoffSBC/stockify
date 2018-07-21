e672fb73c7ad54503db76e70c7c32e11bac8c6a9
TrentonAnk <35786823+TrentonAnk@users.noreply.github.com>
2018-07-21 09:23:15 -0400

    Add files via upload

--- models/db/schema.sql
+++ models/db/schema.sql
@@ -1,24 +1,19 @@
+
 -- Schema
 DROP DATABASE IF EXISTS stock_db;
 CREATE DATABASE stock_db;
 
-USE stock_db;
+-- USE stock_db;
 
--- Our stock table to hold data from IEX API as added from user input
-CREATE TABLE stocks
-(
-    id int NOT NULL AUTO_INCREMENT,
-    symbol varchar(255) NOT NULL,
-    companyName VARCHAR(255),
-    latestPrice INTEGER,
-    high INTEGER,
-    low INTEGER,
-    PRIMARY KEY (id)
-);
+-- -- Our stock table to hold data from IEX API as added from user input
+-- CREATE TABLE stocks
+-- (
+--     id int NOT NULL AUTO_INCREMENT,
+--     symbol varchar(255) NOT NULL,
+--     companyName VARCHAR(255),
+--     high INTEGER(10),
+--     low INTEGER(10),
+--     latestPrice INTEGER(10),
+--     PRIMARY KEY (id)
+-- );
 
--- --------//This is how the info comes in from IEX API//----------
--- symbol string
--- companyName string
--- latestPrice  number
--- related string
--- --------//------------------------------------------//----------
